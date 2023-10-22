a3338fc92138dd04cc3d0725ee089e74f5af64a5
Istam <istam.sadirov@mail.ru>
2023-10-22 15:01:41 +0700

    counter

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_example_documentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_example_documentation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example_documentation/app.dart';
+import 'package:flutter_bloc_example_documentation/counter_observer.dart';
+
+
+void main() {
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
+}
